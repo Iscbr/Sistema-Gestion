ff4448105c48a813c96f4d175d2c0bcc70a9eee6
Iscbr <israelscbr@gmail.com>
2020-09-26 21:24:00 -0500

    Init commit.

---
+++ sgestion-server/src/main/kotlin/com/pp/iescobar/sgestionserver/SgestionServerApplication.kt
@@ -0,0 +1,11 @@
+package com.pp.iescobar.sgestionserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SgestionServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SgestionServerApplication>(*args)
+}
