1135257c9314ed909759224829d37959e7233c9a
Iscbr <b36hQJWZ75D3Mdi>
2020-11-07 18:49:15 -0600

    Se mejora la creación de un artículo.  - Ahora se muestra un toggle para activo.  - Se mejora manejo del modal cuando se manda al detalle después de creación o edición.  - Se mejora el campo "Precio" ahora cuando se ingresa una cantidad se parsea a formato de moneda.

--- sgestion-app/src/app/inventory/inventory-item-create/inventory-item-create.page.ts
+++ sgestion-app/src/app/inventory/inventory-item-create/inventory-item-create.page.ts
@@ -3,6 +3,7 @@ import { ModalController } from "@ionic/angular";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 
+import { UtilCurrencyService } from "../../../services/util/util-currency.service";
 import { UtilUiService } from "../../../services/util/util-ui.service";
 import { ItemService } from "../../../services/item.service";
 
@@ -18,6 +19,7 @@ export class InventoryItemCreatePage implements OnInit {
 
   constructor(
     private modalController: ModalController,
+    private currencyService: UtilCurrencyService,
     private uiService: UtilUiService,
     private router: Router,
     private itemService: ItemService
@@ -27,23 +29,29 @@ export class InventoryItemCreatePage implements OnInit {
       description: new FormControl(null, { validators: Validators.required, updateOn: "blur" }),
       price: new FormControl(null, { validators: Validators.required, updateOn: "blur" }),
       stock: new FormControl(null, { validators: Validators.required, updateOn: "blur" }),
-      // active: new FormControl(true, { updateOn: "blur" })
+      active: new FormControl(true, { updateOn: "blur" })
     });
     this.newItemForm.markAllAsTouched();
   }
 
+  public newItemForm: FormGroup;
   @Input() private idItem: number;
   private item: Item;
-  public newItemForm: FormGroup;
   private editItem: boolean;
+  public title: string;
 
   public async ngOnInit() {
+    await this.uiService.showLoadingAlert("Cargando...");
     if (this.idItem) {
+      this.title = "Editar artículo";
       this.editItem = true;
       await this.loadItem();
     } else {
+      this.title = "Crear artículo";
       this.editItem = false;
+      this.item = new Item();
     }
+    await this.uiService.dismissLoadingAlert();
   }
 
   public async loadItem() {
@@ -65,7 +73,14 @@ export class InventoryItemCreatePage implements OnInit {
     )
   }
 
+  public parsePrice(amount: string) {
+    const amountAsNumber = this.currencyService.getNumberFromCurrencyString(amount.toString());
+    const amountAsCurrencyString = this.currencyService.formatAmountToCurrencyString(amountAsNumber);
+    this.newItemForm.get("price").setValue(amountAsCurrencyString);
+  }
+
   public async onSubmit() {
+    await this.uiService.showLoadingAlert("Guardando...")
     const errorMessage = await this.validateForm();
     if (errorMessage !== "") {
       await this.uiService.showMessageAlert(
@@ -75,18 +90,22 @@ export class InventoryItemCreatePage implements OnInit {
         ["OK"])
     } else {
       if (this.editItem) {
+        // EDITION OF ITEM
         await this.mapFormToItem();
         this.itemService.update(this.idItem, this.item).subscribe(
-          itemUpdated => this.modalController
-            .create({
-              cssClass: "custom-modal-sale-detail",
-              component: InventoryItemDetailPage,
-              componentProps: {
-                item: itemUpdated
-              }
-            })
-            .then(componentCreated => componentCreated.present())
-          ,
+          itemUpdated => {
+            this.modalController
+              .create({
+                cssClass: "custom-modal-sale-detail",
+                component: InventoryItemDetailPage,
+                componentProps: {
+                  item: itemUpdated
+                }
+              })
+              .then(componentCreated => componentCreated.present());
+            this.uiService.dismissLoadingAlert();
+            this.closeModal('DONE');
+          },
           error => this.uiService.showMessageAlert(
             false,
             "Error al actualizar artículo",
@@ -99,17 +118,32 @@ export class InventoryItemCreatePage implements OnInit {
           )
         )
       } else {
+        // CREATION OF ITEM
         await this.mapFormToItem();
         this.itemService.save(this.item).subscribe(
-          itemCreated => this.modalController
-            .create({
-              cssClass: "custom-modal-sale-detail",
-              component: InventoryItemDetailPage,
-              componentProps: {
-                item: itemCreated
-              }
-            })
-            .then(componentCreated => componentCreated.present())
+          itemCreated => {
+            this.modalController
+              .create({
+                cssClass: "custom-modal-sale-detail",
+                component: InventoryItemDetailPage,
+                componentProps: {
+                  item: itemCreated
+                }
+              })
+              .then(componentCreated => componentCreated.present());
+            this.uiService.dismissLoadingAlert();
+            this.closeModal("DONE");
+          },
+          error => this.uiService.showMessageAlert(
+            false,
+            "Error al guardar artículo",
+            "Ocurrió un error al guardar el artículo, es posible que el servidor no esté disponible o " +
+            "revise su conexión e inténtelo más tarde.<br><br>ERROR: " + error.error,
+            [{
+              text: "OK",
+              handler: () => this.router.navigate(["/", "inventory"])
+            }]
+          )
         );
       }
     }
@@ -118,21 +152,22 @@ export class InventoryItemCreatePage implements OnInit {
   private async mapFormToItem() {
     this.item.name = this.newItemForm.get("name").value;
     this.item.description = this.newItemForm.get("description").value;
-    this.item.price = parseFloat(this.newItemForm.get("price").value);
+    this.item.price = this.currencyService.getNumberFromCurrencyString(this.newItemForm.get("price").value);
     this.item.stock = parseInt(this.newItemForm.get("stock").value);
+    this.item.active = this.newItemForm.get("active").value;
     return this.item;
   }
 
   private async validateForm() {
     let message = "";
     if (this.newItemForm.get("name").invalid)
-      message += "- El <b>Nombre del artículo</b> es obligatorio"
+      message += "- El <b>Nombre del artículo</b> es obligatorio.<br>"
     if (this.newItemForm.get("description").invalid)
-      message += "- La <b>descripción del artículo</b> es obligatoria"
+      message += "- La <b>descripción del artículo</b> es obligatoria. <br>"
     if (this.newItemForm.get("price").invalid)
-      message += "- El <b>Precio</b> es obligatorio"
+      message += "- El <b>Precio</b> es obligatorio. <br>"
     if (this.newItemForm.get("stock").invalid)
-      message += "- El <b>Stock</b> es obligatorio"
+      message += "- El <b>Stock</b> es obligatorio. <br>"
     return message;
   }
 
