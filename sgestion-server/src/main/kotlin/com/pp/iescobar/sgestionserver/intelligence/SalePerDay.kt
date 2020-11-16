d67eb3c5ad15c6c1a2c937023d44e566e9fd0eb6
Iscbr <b36hQJWZ75D3Mdi>
2020-11-16 15:42:34 -0600

    SERVER: se renombra campo nuevo en modelo de venta por día: date -> day

--- sgestion-server/src/main/kotlin/com/pp/iescobar/sgestionserver/intelligence/SalePerDay.kt
+++ sgestion-server/src/main/kotlin/com/pp/iescobar/sgestionserver/intelligence/SalePerDay.kt
@@ -1,7 +1,7 @@
 package com.pp.iescobar.sgestionserver.intelligence
 
 data class SalePerDay(
-        var date: String = "",
+        var day: String = "",
         var numOfSales: Int = 0,
         var amount: Double = 0.0,
         var hours: ArrayList<String> = ArrayList()
--- sgestion-server/src/main/kotlin/com/pp/iescobar/sgestionserver/service/IntelligenceService.kt
+++ sgestion-server/src/main/kotlin/com/pp/iescobar/sgestionserver/service/IntelligenceService.kt
@@ -20,7 +20,7 @@ class IntelligenceService @Autowired constructor(
             salePerDay.numOfSales++
             salePerDay.amount += it.total
         }
-        salePerDay.date = date.toString()
+        salePerDay.day = date.toString()
         salePerDay.hours.sort()
         return salePerDay
     }
